c3812652d3a591ff06893b337376682e9d49c7c6
okkdev <dev@stehlik.me>
2022-04-28 14:06:45 +0200

    add comet

--- config/packages.nix
+++ config/packages.nix
@@ -1,10 +1,23 @@
 { config, pkgs, ... }:
 
+let
+  comet = pkgs.stdenv.mkDerivation rec {
+    name = "comet-${version}";
+    version = "0.0.5";
+    src = pkgs.fetchurl {
+      url = "https://github.com/liamg/comet/releases/download/v${version}/comet-darwin-arm64";
+      sha256 = "sha256-UMB7TMhZeVQZVoNZBuUT+0IeaYdeSBk/4mNhdzo+Q4o=";
+    };
+    phases = [ "installPhase" ];
+    installPhase = ''
+      mkdir -p $out/bin
+      cp $src $out/bin/comet
+      chmod +x $out/bin/comet
+    '';
+  };
+in
 {
   nixpkgs.config.allowUnfree = true;
-  home.file.".config/nixpkgs/config.nix".text = ''
-    { allowUnfree = true; }
-  '';
 
   home.packages = with pkgs; [
     # Tools
@@ -13,7 +26,9 @@
     exa
     bat
     viddy
+    jq
     nix-info
+    comet
 
     # Programming
     yarn
@@ -21,6 +36,7 @@
     beam.packages.erlangR24.elixir_1_13
     nodejs-16_x
     rustup
+    go_1_18
 
     # Nix
     cachix
