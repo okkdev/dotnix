24ee84ef9817f977c7156e5210dbbef10f0cf788
okkdev <dev@stehlik.me>
2022-09-22 16:03:52 +0200

    elixir editor vscode shortcut

--- home.nix
+++ home.nix
@@ -7,6 +7,7 @@ with lib; {
     stateVersion = "22.05";
     sessionVariables = {
       ERL_AFLAGS = "-kernel shell_history enabled";
+      ELIXIR_EDITOR = "code --goto __FILE__:__LINE__";
       EDITOR = "nvim";
       BAT_THEME = "ansi";
     };
