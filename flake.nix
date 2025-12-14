6676a23872cda4aa7666fa781512d7c51e39fdc8
okkdev <dev@stehlik.me>
2025-12-14 01:06:19 +0100

    add zen

--- flake.nix
+++ flake.nix
@@ -20,6 +20,11 @@
       url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
   };
 
   outputs =
